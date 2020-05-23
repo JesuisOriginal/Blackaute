152f43b0b09cac840d48ac3476e1b86fd98e0e78
André Oliveira <33301759+JesuisOriginal@users.noreply.github.com>
2020-05-23 16:09:25 -0300

    Added v1.0.0
    Buggy version

---
+++ init-firebase.js
@@ -0,0 +1,14 @@
+require("index.js");
+var firebaseConfig = {
+	apiKey: "AIzaSyAvon5BBiMDjnR_Y0qB053VaSlX99ql6TY",
+	authDomain: "apptestedb.firebaseapp.com",
+	databaseURL: "https://apptestedb.firebaseio.com",
+	projectId: "apptestedb",
+	storageBucket: "apptestedb.appspot.com",
+	messagingSenderId: "61531276055",
+	appId: "1:61531276055:web:d6e3ee46af8bd5de83ed52"
+  };
+  // Initialize Firebase
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
