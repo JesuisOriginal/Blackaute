093607fd5c1557186362dadaaeb060ac39a03b01
Henrique Joaquim <42215737+concrete-kite@users.noreply.github.com>
2020-06-29 20:11:09 -0300

    New Component Beta

--- qr-app/src/pages/MapPage/index.js
+++ qr-app/src/pages/MapPage/index.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import View from "../../components/View";
-import Mapp from "../../components/Mapp";
-import { Height } from '@material-ui/icons';
+//import Mapp from "../../components/Mapp";
+import Pedidos from "../../components/Pedidos";
 
 class MapPage extends Component {
     render() {
         return (
             <View>
-                <Mapp style={{height: '100%', width: '100%'}}/>
+                {/* <Mapp style={{height: '100%', width: '100%'}}/> */}
+                <Pedidos />
             </View>
         )
     }
