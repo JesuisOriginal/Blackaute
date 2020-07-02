c0594277fbced5c63a74be025de8285e762a31df
JesuisOriginal <andreguerra767@gmail.com>
2020-07-02 05:52:26 -0300

    fixed imcompatibility

--- qr-app/src/services/db/utils/geopoint.js
+++ qr-app/src/services/db/utils/geopoint.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase';
 
 export const generate = ({coords}) => (
-    {lat: coords.latitude, long: coords.longitude}
+    {lat: coords.latitude, lng: coords.longitude}
 )
 
 // new firebase.firestore.GeoPoint(position.coords.latitude, position.coords.longitude)
